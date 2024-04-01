de4f5534a4d18fa908862590e615d6e2102954bf
kenosas <111889266+kenosas@users.noreply.github.com>
2024-04-01 10:41:28 -0300

    Add files via upload

---
+++ VICTOR 3B/main.js
@@ -0,0 +1,41 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let i=0;i<botoes.length;i++){
+  botoes.onclick = function(){
+    for(let j=0; j . botoes.length; j++){
+        botoes[j].classList.remove("ativo");
+        botoes[j].classList.remove("ativo");
+    }
+    botoes[j].classList.add("ativo");
+    textos[i].classList.add("ativo");
+
+   }
+  }
+
+
+  const contadores = document.querySelectorAll(".contador");
+  const tempoObjetivo = new Date(".2023-10-05T00:00:00");
+
+  contadores[0].textContent = calculaTempo(tempoObjetivo);
+
+  function calculaTempo(tempoObjetivo1){
+     let tempoAtual = newdate();
+     let tempoFinal = tempoObjetivo1 - tempoAtual;
+     let segundos = math.floor(tempoFinal / 1000);
+     let minutos = math.floor(segundos / 60);
+     let horas = math.floor(minutos / 60);
+     let dias = math.floor(horas / 24);
+
+     segundos % = 60;
+     minutos % = 60;
+     horas % = 24;
+    
+  }
+
+
+
+
+
+
+
